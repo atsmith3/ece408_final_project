a40250f97e6230c9d89e66f7a69028124682cc00
Nick Cebry <ncebry2@illinois.edu>
2019-11-18 15:12:10 -0600

    Change indexing scheme to have extra threads on 2 sides instead of 4

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -23,13 +23,12 @@ __global__ void forward_kernel(float *y, const float *x, const float *k, const i
   The goal here is to be correct AND fast.
   We have some nice #defs for you below to simplify indexing. Feel free to use them, or create your own.
   */
-  __shared__ float in_tile[12][12][12];
+  __shared__ float in_tile[7][12][12];
 
-  const int radius = (K-1)/2;
   // Calculate output X and Y for the thread
-  const int x_out = blockIdx.x*BLOCK + (threadIdx.x - (K-1)/2);
-  const int y_out = blockIdx.y*BLOCK + (threadIdx.y - (K-1)/2);
-  const int c = blockIdx.z*blockDim.z + threadIdx.z;
+  const int tx = blockIdx.x*BLOCK + threadIdx.x;
+  const int ty = blockIdx.y*BLOCK + threadIdx.y;
+  const int tc = blockIdx.z*blockDim.z + threadIdx.z;
 
   const int H_out = H - K + 1;
   const int W_out = W - K + 1;
@@ -41,39 +40,31 @@ __global__ void forward_kernel(float *y, const float *x, const float *k, const i
 #define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
 #define k4d(i3, i2, i1, i0) kernel[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
 
-  const int tx = threadIdx.x - radius;
-  const int ty = threadIdx.y - radius;
-
-  const int ix = threadIdx.x + blockIdx.x*BLOCK;
-  const int iy = threadIdx.y + blockIdx.y*BLOCK;
-  const int ic = threadIdx.z + blockIdx.z*blockDim.z;
-
   float temp = 0;
 
   // Verify Output is Valid
   for(int b = 0; b < B; ++b) {
     // Load Input Tile:
-    if(ix < W && iy < H && ic < C) {
-      in_tile[threadIdx.z][threadIdx.y][threadIdx.x] = x4d(b,ic,iy,ix);
+    if(tx < W && ty < H && tc < C) {
+      in_tile[threadIdx.z][threadIdx.y][threadIdx.x] = x4d(b,tc,ty,tx);
     }
     __syncthreads();
     for(int m = 0; m < M; m++) {
-      if((x_out >= 0 && x_out < W_out) && (y_out >= 0 && y_out < H_out)) {
-        if(((tx) >= 0 && (tx) < BLOCK) && ((ty) >= 0 && (ty) < BLOCK)) {
+      if(tx < W_out && ty < H_out && tc < C) {
+        if(threadIdx.x < BLOCK && threadIdx.y < BLOCK) {
           temp = 0;
-          if(c < C) {
-            for(int p = 0; p < K; p++) {
-              for(int q = 0; q < K; q++) {
-                //y[b][m][y_out][x_out] += x[b][c][y_out + p][x_out + q] * k[m][c][p][q];
-                //temp += x4d(b,c,y_out+p,x_out+q) * k4d(m,c,p,q);
-                temp += in_tile[threadIdx.z][ty+p][tx+q] * k4d(m,c,p,q);
-              }
+          for(int p = 0; p < K; p++) {
+            for(int q = 0; q < K; q++) {
+              //y[b][m][y_out][x_out] += x[b][c][y_out + p][x_out + q] * k[m][c][p][q];
+              //temp += x4d(b,tc,ty+p,tx+q) * k4d(m,tc,p,q);
+              temp += in_tile[threadIdx.z][threadIdx.y+p][threadIdx.x+q] * k4d(m,tc,p,q);
             }
           }
-          atomicAdd(&(y4d(b,m,y_out,x_out)), temp);
+          atomicAdd(&(y4d(b,m,ty,tx)), temp);
         }
       }
     }
+    __syncthreads();
   }
 
 
@@ -102,16 +93,17 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
   const int K = w.shape_[3];
   const int H = x.shape_[2];
   const int W = x.shape_[3];
+  const int H_out = H - K + 1;
+  const int W_out = W - K + 1;
 
-  const int radius = (K-1)/2;
-  const int block = BLOCK + 2*radius;
+  const int block = BLOCK + K - 1;
 
   std::cerr << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n\n\n\n" << B << " " << M  << " " << C << " " << K << " " << H << " " << W << "\n\n\n\n...............................";
 
   // Set the kernel dimensions
   cudaMemcpyToSymbol(kernel, w.dptr_, M*C*K*K*sizeof(float));
-  //cudaMemset((void*)y.dptr_, 0, B*M*H*W*sizeof(float));
-  dim3 gridDim(ceil((float)(W-2*radius)/((float)BLOCK)), ceil((float)(H-2*radius)/((float)BLOCK)), ceil((float)(C)/(float)C_BLOCK));
+  cudaMemset((void*)y.dptr_, 0, B*M*H_out*W_out*sizeof(float));
+  dim3 gridDim(ceil((float)(W-K+1)/((float)BLOCK)), ceil((float)(H-K+1)/((float)BLOCK)), ceil((float)(C)/(float)C_BLOCK));
   dim3 blockDim(block, block, C_BLOCK);
 
   // Call the kernel
